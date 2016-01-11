3b5f7468c498c301fdbfb467eb0daa275f88b5eb
vitush <vitush@maven.co>
2016-01-11 21:36:14 +0200

    Typo fixed

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
-from .mailjson import __version__ as version
+from mailjson import __version__ as version
 
 CLASSIFIERS = """\
 Development Status :: 4 - Beta
